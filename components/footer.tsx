630800f72334a100436ed8ad9de33f06dbf5c195
Rishi Raj Jain <rishi@neon.tech>
2024-11-11 12:57:24 +0530

    lazy load logo

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export default function () {
     <footer className="mt-2 py-4 border-t border-white/10">
       <div className="flex flex-col sm:flex-row items-center sm:justify-between max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <Link href="/">
-          <img src={config.logo} width={158} height={48} className="h-[30px] w-auto" alt="Neon Logo" />
+          <img loading="lazy" decoding="async" src={config.logo} width={158} height={48} className="h-[30px] w-auto" alt="Neon Logo" />
         </Link>
         <a
           target="_blank"
